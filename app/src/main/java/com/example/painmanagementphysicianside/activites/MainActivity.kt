3ab3f389ccc78ed39ab40bca1132d16d5e076fd6
Ulico <aruss8@illinois.edu>
2022-02-17 14:39:16 -0600

    added some MongoDB integration

--- app/src/main/java/com/example/painmanagementphysicianside/activites/MainActivity.kt
+++ app/src/main/java/com/example/painmanagementphysicianside/activites/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.painmanagementphysicianside.R
+import io.realm.Realm
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+import io.realm.mongodb.Credentials
+import io.realm.mongodb.User
 
+lateinit var taskApp: App
 class MainActivity : AppCompatActivity() {
+    val appID = "test_app-svywj"
+    var user: User? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Realm.init(this)
+        taskApp = App(
+            AppConfiguration.Builder(appID)
+                .build()
+        )
+
     }
 
     fun onLogIn(view: View) {
-        val intent = Intent(this, TwoFactorAuthentication::class.java)
-        startActivity(intent)
+        val emailPasswordCredentials: Credentials = Credentials.emailPassword(
+            "aruss8@illinois.edu",
+            "test123"
+        )
+
+        taskApp.loginAsync(emailPasswordCredentials) {
+            if (it.isSuccess) {
+                user = taskApp.currentUser()
+
+
+                val intent = Intent(this, TwoFactorAuthentication::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
     fun onHelp(view: View) {
