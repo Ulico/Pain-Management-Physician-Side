cf9e838b59404baa82df6002689fc21311a42a5b
Ulico <aruss8@illinois.edu>
2022-02-18 16:17:00 -0600

    encouraged Portrait layout

--- app/src/main/java/com/example/painmanagementphysicianside/activites/Homescreen.kt
+++ app/src/main/java/com/example/painmanagementphysicianside/activites/Homescreen.kt
@@ -1,9 +1,9 @@
 package com.example.painmanagementphysicianside.activites
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.painmanagementphysicianside.R
 
 class Homescreen : AppCompatActivity() {
@@ -16,11 +16,13 @@ class Homescreen : AppCompatActivity() {
         val intent = Intent(this, Patients::class.java)
         startActivity(intent)
     }
+
     fun onData(view: View) {}
     fun onMessages(view: View) {
         val intent = Intent(this, Messages::class.java)
         startActivity(intent)
     }
+
     fun onAccount(view: View) {}
     fun onLogout(view: View) {
         val intent = Intent(this, MainActivity::class.java)
